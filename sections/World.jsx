24804cf07e41148051591faf1123d413dbcac3a2
ransiSe7en <ransihowler@gmail.com>
2023-09-13 13:08:23 +0530

    added gradient

--- sections/World.jsx
+++ sections/World.jsx
@@ -8,6 +8,7 @@ import { staggerContainer, fadeIn } from '../utils/motion';
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
     <motion.div
       variants={staggerContainer}
       initial="hidden"
