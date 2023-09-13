663b4127471f1d37708503dace365470213b685a
ransiSe7en <ransihowler@gmail.com>
2023-09-13 10:26:57 +0530

    whats new edit

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import styles  from '../styles';
-import { staggerContainer, fadeIn, planetVariants} from '../utils/motion';
-import { NewFeatures, TitleText, TypingText } from "../components";
-import { newFeatures } from "../constants";
+import styles from '../styles';
+import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -13,22 +13,18 @@ const WhatsNew = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{once: 'false', amount:0.25}}
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
-      
       <motion.div
-        variants={fadeIn('right','tween',0.2,1)}
-        className="flex-[0.75] flex justify-center flex-col"
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.95] flex justify-center flex-col"
       >
-        <TypingText title="| Whats new"/>
-        <TitleText title={<>What's new about Metaversus </>} />
+        <TypingText title="| Whats new?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
         <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
-          {newFeatures.map((feature, index) => (
-            <NewFeatures
-              key={feature.title}
-              {...feature}
-            />
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
           ))}
         </div>
       </motion.div>
