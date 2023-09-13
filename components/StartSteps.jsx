b775aeb3a521f2283d67bff2ffc8d033fedf9efb
ransiSe7en <ransihowler@gmail.com>
2023-09-13 17:03:47 +0530

    deploy errors edit

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,6 @@
-import styles from "../styles";
+import styles from '../styles';
 
-const StartSteps = ({number, text}) => (
+const StartSteps = ({ number, text }) => (
   <div className={`${styles.flexCenter} flex-row`}>
     <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
       <p className="font-bold text-[20px] text-white">0{number}</p>
