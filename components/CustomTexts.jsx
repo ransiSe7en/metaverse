4836bd5a1c77342160eb65b44bf6cb8452580ddc
ransiSe7en <ransihowler@gmail.com>
2023-09-13 17:48:02 +0530

    build req

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -3,7 +3,6 @@
 import { motion } from 'framer-motion';
 import { textContainer, textVariant2 } from '../utils/motion';
 
-
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
